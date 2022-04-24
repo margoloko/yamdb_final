0f95f8b24d0aa25e97a361e1a9cbf85175cb0aae
Marina <margoloko@mail.ru>
2022-04-24 20:50:30 +0700

    pep8

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,5 +1,5 @@
-from rest_framework.permissions import SAFE_METHODS
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS
 
 
 class ReadOnly(permissions.BasePermission):
